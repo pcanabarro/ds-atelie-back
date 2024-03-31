b807ffbdafaec4d0a1c643cda966b1700c84d91e
pcanabarro <pedroh.canabarro@gmail.com>
2024-03-31 13:10:46 -0300

    Seed methods

--- src/connections/database.js
+++ src/connections/database.js
@@ -28,6 +28,49 @@ class Database {
       client.release()
     }
   }
+
+  async seed() {
+    try {
+      await this.query("INSERT INTO Categorias (nome) VALUES ('Joias')")
+      await this.query("INSERT INTO Categorias (nome) VALUES ('Acessórios')")
+      await this.query("INSERT INTO Categorias (nome) VALUES ('Produtos Masculinos')")
+
+      await this.query("INSERT INTO Produtos (nome, categoria_id, material, preco) VALUES ('Colar de Diamante', 1, 'Diamante', 5000.00)")
+      await this.query("INSERT INTO Produtos (nome, categoria_id, material, preco) VALUES ('Cordinha para Óculos', 2, 'Couro', 20.00)")
+      await this.query("INSERT INTO Produtos (nome, categoria_id, material, preco) VALUES ('Relógio de Pulso', 3, 'Aço Inoxidável', 200.00)")
+
+      console.log('Seed method executed successfully')
+    } catch (err) {
+      console.error('Error executing seed method', err)
+    }
+  }
+
+  async createTables() {
+    try {
+      await this.query(`
+        CREATE TABLE IF NOT EXISTS Categorias (
+          id SERIAL PRIMARY KEY,
+          nome VARCHAR(50)
+        )
+      `)
+
+      await this.query(`
+        CREATE TABLE IF NOT EXISTS Produtos (
+          id SERIAL PRIMARY KEY,
+          nome VARCHAR(100),
+          categoria_id INT,
+          material VARCHAR(50),
+          preco DECIMAL(10, 2),
+          FOREIGN KEY (categoria_id) REFERENCES Categorias(id)
+        )
+      `)
+
+      console.log('Tables created successfully')
+    } catch (err) {
+      console.error('Error creating tables', err)
+    }
+  }
+
 }
 
 export default new Database
--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,12 +2,13 @@ import Database from '../connections/database.js'
 import constants from '../utils/constants.js'
 
 export default class ProductsController {
-  static async getProducts = (req, res) => {
+  static getProducts = async (req, res) => {
     try {
       result = await Database.query(constants.QUERIES.ALL_PRODUCTS)
       res.status(200).json({ data: result.rows })
     } catch (e) {
       console.error('error fetching products list', e.message)
+      res.status(500).json({ data: e })
     }
   }
 
@@ -15,9 +16,11 @@ export default class ProductsController {
     const { product } = req.body
     try {
       Database.query(constants.QUERIES.CREATE_PRODUCT, (product.name, product.price))
+      console.log(`product ${product} created!`)
       res.status(200).json({ data: "Product Created" })
     } catch (e) {
       console.error('error fetching products list', e.message)
+      res.status(500).json({ data: e })
     }
   }
 }
